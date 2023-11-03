283acfd653e5c70983afa0512d3799036800d95b
Ismahan <main@ismahans-mbp.lan>
2023-11-03 15:50:03 -0400

    added : Login Scenario feature

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -12,6 +12,8 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/signUp.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/login.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
 public class RunCucumberTest {
 }
