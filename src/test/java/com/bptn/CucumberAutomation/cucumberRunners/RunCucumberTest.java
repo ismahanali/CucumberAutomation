eb2a387a6d14c2ceb0182726b039139f40f693ca
Ismahan <main@ismahans-mbp.lan>
2023-11-03 14:56:10 -0400

    added strongPasswords feature and test

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -5,13 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
 @SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/strongPassword.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
 public class RunCucumberTest {
 }
