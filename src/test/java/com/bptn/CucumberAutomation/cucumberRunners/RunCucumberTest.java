8efb8a3f1773e73bb5a8dc8b157063a1a1f5178c
Ismahan <main@ismahans-mbp.lan>
2023-11-03 14:28:58 -0400

    added calculator feature

--- src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/CucumberAutomation/cucumberRunners/RunCucumberTest.java
@@ -1,15 +1,17 @@
 package com.bptn.CucumberAutomation.cucumberRunners;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("com/bptn/CucumberAutomation")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/example.feature")
+@SelectClasspathResource("com/bptn/CucumberAutomation/features/calculator.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.CucumberAutomation")
 public class RunCucumberTest {
 }
--- src/test/resources/com/bptn/CucumberAutomation/features/example.feature
+++ src/test/resources/com/bptn/CucumberAutomation/features/example.feature
@@ -2,7 +2,7 @@ Feature: An example
 
   Scenario: The example
     Given an example scenario
-     When all step definitions are implemented
-      And additional steps are added
-      But unnecessary steps are removed
-     Then the scenario passes
+    When all step definitions are implemented
+    And additional steps are added
+    But unnecessary steps are removed
+    Then the scenario passes
