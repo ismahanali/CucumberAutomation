c6a51651a77ab7e9bbc37c842edcc85346c6381f
Ismahan <main@ismahans-mbp.lan>
2023-11-06 22:59:57 -0500

    CucumberAutomation - Resource Provide

---
+++ src/main/java/com/bptn/CucumberAutomation/provider/ResourceProvider.java
@@ -0,0 +1,5 @@
+package com.bptn.CucumberAutomation.provider;
+
+public class ResourceProvider {
+
+}
---
+++ src/main/java/com/bptn/CucumberAutomation/provider/factory/YamlPropertySourceFactory.java
@@ -0,0 +1,17 @@
+package com.bptn.CucumberAutomation.provider.factory;
+
+import java.io.IOException;
+
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+	@Override
+	public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
